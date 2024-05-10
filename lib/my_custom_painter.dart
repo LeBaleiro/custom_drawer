9d91ad2e40378ee9208080a183121e658b3322b2
LeBaleiro <lehbaleiro@gmail.com>
2024-05-10 19:10:39 -0300

    clean the code

--- lib/my_custom_painter.dart
+++ lib/my_custom_painter.dart
@@ -3,73 +3,69 @@ import 'package:flutter/material.dart';
 class MyCustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    // Layer 1
-
     const drawerWidth = 0.5;
     const xCutStartPoint = 0.2;
     const yCutStartPoint = 0.2;
     const cutHeight = 0.07;
     const radiusHelper = .01;
 
-    Paint paint_fill_0 = Paint()
+    final paint = Paint()
       ..color = const Color.fromARGB(255, 255, 255, 255)
       ..style = PaintingStyle.fill
       ..strokeWidth = size.width * 0.0
       ..strokeCap = StrokeCap.butt
       ..strokeJoin = StrokeJoin.miter;
 
-    Path path_0 = Path();
-    path_0.lineTo(size.width * drawerWidth, size.height * 0);
-    path_0.lineTo(size.width * drawerWidth,
+    final path = Path();
+    path.lineTo(size.width * drawerWidth, size.height * 0);
+    path.lineTo(size.width * drawerWidth,
         size.height * (yCutStartPoint - radiusHelper));
 
-    path_0.quadraticBezierTo(
+    path.quadraticBezierTo(
       size.width * drawerWidth,
       size.height * yCutStartPoint,
       size.width * (drawerWidth - radiusHelper),
       size.height * yCutStartPoint,
     );
-    path_0.lineTo(size.width * (xCutStartPoint + radiusHelper),
+    path.lineTo(size.width * (xCutStartPoint + radiusHelper),
         size.height * yCutStartPoint);
-    path_0.quadraticBezierTo(
+    path.quadraticBezierTo(
       size.width * xCutStartPoint,
       size.height * yCutStartPoint,
       size.width * xCutStartPoint,
       size.height * (yCutStartPoint + radiusHelper),
     );
-    path_0.lineTo(size.width * xCutStartPoint,
+    path.lineTo(size.width * xCutStartPoint,
         size.height * (yCutStartPoint + cutHeight - radiusHelper));
-    path_0.quadraticBezierTo(
+    path.quadraticBezierTo(
       size.width * xCutStartPoint,
       size.height * (yCutStartPoint + cutHeight),
       size.width * (xCutStartPoint + radiusHelper),
       size.height * (yCutStartPoint + cutHeight),
     );
-    path_0.lineTo(size.width * (drawerWidth - radiusHelper),
+    path.lineTo(size.width * (drawerWidth - radiusHelper),
         size.height * (yCutStartPoint + cutHeight));
-    path_0.quadraticBezierTo(
+    path.quadraticBezierTo(
       size.width * drawerWidth,
       size.height * (yCutStartPoint + cutHeight),
       size.width * drawerWidth,
       size.height * (yCutStartPoint + cutHeight + radiusHelper),
     );
-    path_0.lineTo(size.width * drawerWidth, size.height);
-    path_0.lineTo(size.width * 0, size.height * 1);
-    path_0.lineTo(size.width * 0, size.height * 0);
-    path_0.close();
+    path.lineTo(size.width * drawerWidth, size.height);
+    path.lineTo(size.width * 0, size.height * 1);
+    path.lineTo(size.width * 0, size.height * 0);
+    path.close();
 
-    canvas.drawPath(path_0, paint_fill_0);
+    canvas.drawPath(path, paint);
 
-    // Layer 1
-
-    Paint paint_stroke_0 = Paint()
+    Paint paintStroke = Paint()
       ..color = const Color.fromARGB(255, 33, 150, 243)
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.0
       ..strokeCap = StrokeCap.butt
       ..strokeJoin = StrokeJoin.miter;
 
-    canvas.drawPath(path_0, paint_stroke_0);
+    canvas.drawPath(path, paintStroke);
   }
 
   @override
